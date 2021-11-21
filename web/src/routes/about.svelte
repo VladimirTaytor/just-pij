c6eb4bec3c829bd483ae35f11b85832b38b6ff2e
Volodymyr Pihol <volodymyr.pihol@gmail.com>
2021-11-21 18:16:51 +0100

    Add profile image

--- web/src/routes/about.svelte
+++ web/src/routes/about.svelte
@@ -12,11 +12,49 @@
     return new Date(today - new Date(yearOfBirth)).getFullYear() - offset
   }
 </script>
+<style>
+  img {
+    width: 350px;
+    border: 1px solid var(--primary__color_light);
+  }
+
+  .content {
+    margin-left: 25px;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+
+  @media (max-width: 700px) {
+    .container-flex {
+      flex-direction: column;
+    }
+
+    .content {
+      margin-left: 0;
+      margin-top: 25px;
+    }
+  }
+</style>
 
 <h1>{$_('about.title')}</h1>
 
-<p>{$_('about.hello')}</p>
-<p>{$_('about.me', { values: {age: getMyAge()}})}</p>
-<p>{$_('about.enjoy')}</p>
-<br/>
-<p>{@html $_('about.questions')}</p>
+<div class="container-flex">
+  <img alt="art" src="/images/profile pic.jpg"/>
+  <div class="content">
+    <p>{$_('about.hello')}</p>
+    <p>{$_('about.me', { values: {age: getMyAge()}})}</p>
+    <p>{$_('about.enjoy')}</p>
+    <br/>
+    <p>{@html $_('about.questions')}</p>
+    <hr/>
+    <div class="socials">
+      <a href="https://telegram.me/vovapihol">Telegram</a>
+      <a href="https://github.com/VladimirTaytor">GitHub</a>
+      <a href="https://https://www.instagram.com/vovapihol">Instagram</a>
+    </div>
+  </div>
+</div>
