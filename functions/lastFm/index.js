cf09cb0cd514d64c7977d22711a0465da09980f8
Volodymyr Pihol <volodymyr.pihol@gmail.com>
2021-12-24 22:32:02 +0100

    Allow cors requests for lastFm function

--- functions/lastFm/index.js
+++ functions/lastFm/index.js
@@ -25,6 +25,9 @@ exports.handler = async function(event, context) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World", nowPlaying })
+    body: JSON.stringify({ message: "Hello World", nowPlaying }),
+    headers: {
+      "access-control-allow-origin": "*"
+    }
   };
 }
